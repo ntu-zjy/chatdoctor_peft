8867cff0db885c6ff62ec8e3dffcd43a0b27148e
Jingyuan Zhang <67254387+ntu-zjy@users.noreply.github.com>
2023-11-10 16:30:45 +0800

    Update save_pretrained.py

--- save_pretrained.py
+++ save_pretrained.py
@@ -3,11 +3,11 @@ from transformers import AutoTokenizer, AutoModelForCausalLM, DataCollatorForSeq
 
 model = AutoModelForCausalLM.from_pretrained("linhvu/decapoda-research-llama-7b-hf", low_cpu_mem_usage=True)
 
-save_directory = "./chatdoctor_base/pretrained_weight/LLM_weight"
+save_directory = "./chatdoctor_base/pretrained_weight"
 model.save_pretrained(save_directory)
 
 
 
-tokenizer_save_directory = "./chatdoctor_base/pretrained_weight/tokenizer_weight/"
+tokenizer_save_directory = "./chatdoctor_base/pretrained_weight"
 tokenizer = LlamaTokenizer.from_pretrained("linhvu/decapoda-research-llama-7b-hf")
 tokenizer.save_pretrained(tokenizer_save_directory)
